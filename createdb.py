0c3a2889706236c58f81da17d5ab87c00bfff97d
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-07 15:10:11 +0200

    Now you can actually log in more than once too

--- createdb.py
+++ createdb.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
+from flask.ext.security.utils import encrypt_password
 from datetime import datetime
-from korform2 import db, user_datastore
+from korform2 import *
 
-db.create_all()
+with app.test_request_context():
+	# Create database tables
+	db.create_all()
 
-# Create default roles
-admin_role = user_datastore.create_role(name="Admin")
+	# Create default roles
+	admin_role = user_datastore.create_role(name="Admin")
 
-# Create a default admin account
-admin_user = user_datastore.create_user(email='admin@localhost', password='password', active=True, confirmed_at=datetime.utcnow())
+	# Create a default admin account
+	admin_user = user_datastore.create_user(email='admin@localhost', password=encrypt_password('password'), active=True, confirmed_at=datetime.utcnow())
 
-# Make the default admin an admin
-user_datastore.add_role_to_user(admin_user, admin_role)
+	# Make the default admin an admin
+	user_datastore.add_role_to_user(admin_user, admin_role)
 
-# Save changes
-db.session.commit()
+	# Save changes
+	db.session.commit()
