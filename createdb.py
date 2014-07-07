85ccdf33bf0f57ddcedb6f25546e0ca64bbf58c7
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-07 22:18:05 +0200

    Default Group objects

--- createdb.py
+++ createdb.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# coding=utf-8
 from flask.ext.security.utils import encrypt_password
 from datetime import datetime
 from korform2 import *
@@ -12,9 +13,22 @@ with app.test_request_context():
 
 	# Create a default admin account
 	admin_user = user_datastore.create_user(email='admin@localhost', password=encrypt_password('password'), active=True, confirmed_at=datetime.utcnow())
-
-	# Make the default admin an admin
 	user_datastore.add_role_to_user(admin_user, admin_role)
+	
+	# Create default groups
+	groupdata = [
+		(u'f0',  u'F asp',	u'Flickkörens aspiranter'),
+		(u'f1',	 u'YF',		u'Yngre Flickkören'),
+		(u'f2',	 u'ÄF',		u'Äldre Flickkören'),
+		(u'g0',	 u'G asp',	u'Gosskörens aspiranter'),
+		(u'g1',	 u'GK',		u'Gosskören'),
+		(u'g1n', u'NI',		u'Nissarna'),
+		(u'zgu', u'GU',		u'Göteborgs Ungdomskör')
+	]
+	for data in groupdata:
+		group = Group(sortcode=data[0], code=data[1], name=data[2])
+		db.session.add(group)
+		db.session.commit()
 
 	# Save changes
 	db.session.commit()
