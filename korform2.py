eb67ff7801ae13e0a180ef5ebc3f23e8f755d032
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-07 14:54:03 +0200

    Message Flashing

--- korform2.py
+++ korform2.py
@@ -17,6 +17,14 @@ admin.init_app(app)
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+@app.context_processor
+def context_processor():
+	suffix_overrides = { 'message': 'info' }
+	return {
+		'alert_class_suffix_for_message_category':
+			lambda cat: suffix_overrides[cat] if cat in suffix_overrides else cat
+	}
+
 @app.route('/')
 def index():
 	return render_template("index.html")
