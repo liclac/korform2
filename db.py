9623043fa0133f68f58d83d2b20bc28683d2204b
Johannes Ekberg <uppfinnarn@gmail.com>
2014-07-19 00:53:21 +0200

    Oops, inverted

--- db.py
+++ db.py
@@ -140,10 +140,10 @@ class OSA(db.Model):
 	korist = db.relationship('Korist', backref='osas')
 	
 	# UI
-	osa_strs = ['NULL', 'Nej', 'Ja', 'Kanske']
+	osa_strs = ['NULL', 'Ja', 'Nej', 'Kanske']
 	osa_str = lambda self: self.osa_strs[self.osa]
 	
-	osa_class_suffixes = ['unknown', 'danger', 'success', 'warning']
+	osa_class_suffixes = ['unknown', 'success', 'danger', 'warning']
 	osa_class_suffix = lambda self: self.osa_class_suffixes[self.osa]
 	
 	def __str__(self):
