9edf122866d2761df2246b0a4752524c15b47c8e
uppfinnarn <uppfinnarn@gmail.com>
2015-07-12 12:03:58 +0200

    SQLite isn't working properly

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.abspath(os.path.dirname(__file__)) + '/korform2.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.abspath(os.path.dirname(__file__)) + '/korform2.db'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2:///korform2'
 
 SECURITY_PASSWORD_HASH = 'pbkdf2_sha512'
 SECURITY_CONFIRMABLE = True
